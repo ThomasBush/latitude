d147bf8e5f8ffa92ae2fca6f85444b04eb20477a
Thomas Bush <ThomasJosephBush@gmail.com>
2013-12-21 19:42:02 -0500

    Latitude first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Latitude::Application.config.secret_key_base = '006f5b18ab55c6deadcd41556f132c45361fff0d3f0162c2accb93fb240f680bebe3380c7412bfd0cb2585d90ab78fc7a3c31ea33a175f6bb1bf544fd5a3a175'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Latitude::Application.config.session_store :cookie_store, key: '_latitude_session'
